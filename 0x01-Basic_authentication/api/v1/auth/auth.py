22761c5e5a1b9b89572cd1c082cd0f7d4901e121
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-04 11:19:10 +0100

    Auth class for all authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -5,7 +5,7 @@ application
 from flask import request
 
 
-class Auth:
+class Auth():
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Required Authentication """
         returns False
