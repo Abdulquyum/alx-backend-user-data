469925ac97ff752328c7b66eeaba435fc69b2f8d
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-04 12:38:55 +0100

    Auth class for all authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,13 @@ class Auth():
         Returns:
             bool: True if the path requires authentication, False otherwise.
         """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path in excluded_paths:
+            return False
+        return True
 
 
     def authorization_header(self, request=None) -> str:
