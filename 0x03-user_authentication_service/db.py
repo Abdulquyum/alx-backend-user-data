b5b7e86545ba8d60e40cf7f75a438035eec25524
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-13 12:46:06 +0100

    Create User model with add user and find user methods

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -6,6 +6,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from user import Base, User
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 
 class DB():
@@ -33,3 +34,16 @@ class DB():
         self._session.commit()
 
         return new_user
+
+    def find_user_by(self, **kwargs):
+        """ Find USer by """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user:
+                return user
+            else:
+                raise NoResultFound
+        except NoResultFound as e:
+            raise e
+        except InvalidRequestError as err:
+            raise err
