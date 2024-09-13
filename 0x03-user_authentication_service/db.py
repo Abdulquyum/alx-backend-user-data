e7aeafb2da00335316f9417a407236971847a4f2
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-13 17:41:32 +0100

    Create User model with add user and find user methods

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,7 +53,7 @@ class DB():
         """ Update user info """
         try:
             user = self.find_user_by(id=user_id)
-            
+
             for key, value in kwargs.items():
                 if hasattr(user, key):
                     setattr(user, key, value)
