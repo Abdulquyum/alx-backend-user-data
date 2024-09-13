c85dbad5864ea251c3acb1cd70bbea59696dfa2e
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-13 11:51:39 +0100

    Create user and add session to database

---
+++ 0x03-user_authentication_service/db.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""DB Module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
+
+class DB():
+    """ DB Class """
+    def __init__(self) -> None:
+        """ Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
+
+
+    @property
+    def _session(self) -> Session:
+        """ Memorize session object
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+
+    def add_user(self, email: str, hashed_password: str) -> 'User':
+        """ Save Users to DB """
+        new_user = User(email=email, hashed_password=hashed_password)
+
+        self._session.add(new_user)
+        self._session.commit()
+
+        return new_user
