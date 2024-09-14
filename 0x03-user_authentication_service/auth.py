69ce604f9edfad790d2a0ff902db60abace5c296
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-14 05:44:58 +0100

    Autheticate and hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -19,19 +19,3 @@ class Auth:
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
 
         return hashed_password
-
-    def register_user(self, email: str, password: str) -> 'User':
-        """ Register new users """
-        try:
-            user_exist = self._db.find_user_by(email=email)
-            if user_exist:
-                raise ValueError(f'User {email} already exists')
-
-        except NoResultFound:
-            hashed_password = self._hash_password(password)
-
-            new_user = User(email=email, hashed_password=hashed_password)
-
-            self._db.add_user(new_user)
-
-            return new_user
