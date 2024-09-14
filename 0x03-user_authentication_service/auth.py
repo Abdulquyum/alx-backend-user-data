6b90b1010877ef67d189cfda2f8d27226da7bcb2
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-14 05:46:27 +0100

    Autheticate and hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,19 +1,9 @@
 #!/usr/bin/env python3
 """ Auth file """
 import bcrypt
-from db import DB
-from user import User
-from sqlalchemy.orm.exc import NoResultFound
 
 
-class Auth:
-    """ Auth class to interact with the authentication database.
-    """
-
-    def __init__(self):
-        self._db = DB()
-
-    def _hash_password(password: str) -> bytes:
+def _hash_password(password: str) -> bytes:
         """ Hash password """
         salt = bcrypt.gensalt()
         hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
