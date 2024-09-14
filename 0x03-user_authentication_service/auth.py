6179ebe12ee5bd85097eb7e7fd31970e2f06e9d3
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-14 05:10:28 +0100

    Autheticate and hash password

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 """ Auth file """
 import bcrypt
+from db import DB
+from user import User
 
+class Auth:
+    """ Auth class to interact with the authentication database.
+    """
 
-def _hash_password(password: str) -> bytes:
-    """ Hash password """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+    def __init__(self):
+        self._db = DB()
 
-    return hashed_password
+    def _hash_password(self, password: str) -> bytes:
+        """ Hash password """
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        return hashed_password
+
+    def register_user(self, email: str, password: str) -> 'User':
+        """ Register new users """
+        if email in User:
+            raise ValueError('User {User[email]} already exists')
+
+        user = self._hash_password(password)
+
+        self._db(user)
+
+        return user
