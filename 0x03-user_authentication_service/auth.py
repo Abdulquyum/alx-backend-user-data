efa1bb9d5bd6e6ae977fe7efdfc9a922d785f50d
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-03-11 09:35:58 +0000

    authentication file

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,11 +1,62 @@
 #!/usr/bin/env python3
 """ Auth file """
+
 import bcrypt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 
-def _hash_password(password: str) -> bytes:
-    """ Hash password """
-    salt = bcrypt.gensalt()
-    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+class Auth:
+    """Auth class to interact with the authentication database"""
 
-    return hashed_password
+    def __init__(self):
+        """imported db class"""
+        self._db = DB()
+
+    def register_user(self, email, password) -> User:
+        """help register method"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exist")
+
+        except NoResultFound as err:
+            hashed_password = self._hash_password(password)
+            new_user = self._db.add_user(email, hashed_password)
+
+            return new_user
+
+    def valid_login(self, email, password) -> bool:
+        """Check if user exist"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email) -> str:
+        """create session """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = self._generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+
+            return session_id
+
+        except NoResultFound:
+            return None
+
+    @staticmethod
+    def _generate_uuid() -> str:
+        """generate uuid for user"""
+        return str(uuid.uuid4())
+
+    @staticmethod
+    def _hash_password(password: str) -> bytes:
+        """Hash password """
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+        return hashed_password
+
