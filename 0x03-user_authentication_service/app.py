55fe6e41b8f12ed88f3b135ff94cfbdd2ea65d75
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-03-11 09:36:44 +0000

    flask user authentication app

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,18 +1,54 @@
 #!/usr/bin/env python3
-""" Basic Flask app """
+"""Basic Flask app"""
 
-
-from flask import Flask, jsonify
-
+from flask import Flask, jsonify, request, abort
+from auth import Auth
+from sqlalchemy.orm.exc import NoResultFound
+import uuid
 
 app = Flask(__name__)
 
+AUTH = Auth()
 
-@app.route('/', methods=['GET'], strict_slashes=False)
-def root():
-    """ Root Address """
+@app.route("/", methods=['GET'], strict_slashes=False)
+def basic():
+    """basic route"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=['POST'], strict_slashes=False)
+def users():
+    """Register user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        AUTH.register_user(email, password)
+        return jsonify({
+            "email": email,
+            "message": "user created"
+            })
+    except ValueError as e:
+        return jsonify({
+            "message": "email already registered"
+            }), 400
+
+@app.route("/sessions", methods=['POST'], strict_slashes=False)   
+def login():
+    """Log in authorized user"""
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        if AUTH.valid_login(email, password):
+            session_id = AUTH.create_session(email)
+            response = jsonify({"email": email, "message": "logged in"})
+            response.set_cookie('session_id', session_id)
+            return response
+                
+        abort(401)
+            
+    except NoResultFound:
+            abort(401)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port="5000")
+
