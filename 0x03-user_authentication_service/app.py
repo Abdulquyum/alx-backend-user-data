96944541145c39d9459ba5999f9bdcafffdaafe2
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-03-13 09:48:16 +0000

    flask user auth application

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -49,6 +49,21 @@ def login():
     except NoResultFound:
             abort(401)
 
+@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
+def logout():
+    """Delete session"""
+    session_id = request.cookies.get('session_id')
+    if session_id is None:
+        abort(403)
+    try:
+        user = AUTH.get_user_from_session_id(session_id)
+        if user:
+            AUTH.destroy_session(user.id)
+            return redirect("/")
+        abort(403)
+    except NoResultFound:
+        abort(403)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port="5000")
 
