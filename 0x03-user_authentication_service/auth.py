7f9c04a085adec626fa0a21013ba53b1f14a1d4c
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-03-13 09:49:01 +0000

    helping function to authenticate users

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -47,6 +47,24 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get user By session ID"""
+        if session_id is None:
+            return None
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy user session"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+            return None
+        except NoResultFound:
+            return None
+
     @staticmethod
     def _generate_uuid() -> str:
         """generate uuid for user"""
