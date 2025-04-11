4d462f6a06fcda0278498076330bbaf84b4e5ec6
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-04-11 10:16:06 +0100

    basic function needs to obfuscate specific fields in a log message using regex substitution

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,15 +4,15 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Obfuscate specified fields in a log message.
-    
     Args:
         fields: List of field names to obfuscate
         redaction: String to replace field values with
         message: The log message to process
         separator: Character separating fields in the log message
-        
     Returns:
         The obfuscated log message
     """
