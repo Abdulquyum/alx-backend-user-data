0971dc45f951a6e2e271975b8c6c1cf8168a7a8f
Abdulquyum <ajumobiabdulquyum@gmail.com>
2025-04-13 17:42:48 +0100

    flask app for user auth and it helping fucntions

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,7 +4,7 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declarative_base
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
